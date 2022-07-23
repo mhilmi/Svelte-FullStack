9b9dc535c051ed835bb0c4721c36aa8a0da31ae7
Mahmoud Sharef <mhelmy@usa.com>
2022-07-23 03:06:55 +0300

    Initialize mikenikles/develop-html-structure-5

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+  import TodoItem from "$lib/todo-item.svelte";
+
+  const title = "Todo";
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="">
+  <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo" />
+</form>
+
+<TodoItem />
+<TodoItem />
+<TodoItem />
